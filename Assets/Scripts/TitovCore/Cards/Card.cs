85412cb6262ea28824180e93755b4b9092808958
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2025-01-07 07:21:24 -0500

    Card stores reference to its behavior script
    Deep Copies based on other cards

--- Assets/Scripts/TitovCore/Cards/Card.cs
+++ Assets/Scripts/TitovCore/Cards/Card.cs
@@ -11,10 +11,12 @@ namespace TitovCore.Cards
         public string code;
         public CardType type;
         public string flavorText;
+        public Type cardScript; //reference to the Monobehavior script for this card. 
+        public GameObject cardPrefab; //reference to the gameobject template for this card. 
         
        // Since this is the universal    
 
-        public Card(string _title, string _code, string _flavorText=null)
+        public Card(string _title, string _code, string _flavorText=null, Type _cardScript=null)
         {
             if (_code[0] == 'M') type = CardType.Milestone;
             else if (_code[0] == 'C') type = CardType.Action;
@@ -23,6 +25,53 @@ namespace TitovCore.Cards
             title = _title;
             code = _code;
             flavorText = _flavorText;
+            cardScript = _cardScript;
+            
+            if (_cardScript != null)
+            {
+                Debug.Log("Card::Card -- Script type passed: " + _cardScript);
+            }
+        }
+
+        
+        //Deep copies
+
+        public Card(Card _card)
+        {
+            type = _card.type;
+            title = _card.title;
+            code = _card.code;
+            flavorText = _card.flavorText ?? "";
+            cardScript = _card.cardScript;
+            cardPrefab = _card.cardPrefab;
+        }
+
+        public Card(string cardCode)
+        {
+            try
+            {
+                int pos = Deck.deckLookup[cardCode];
+                Card _card = null;
+
+                if (cardCode[0] == 'M') _card = Deck.AllMilestones[pos];
+                else if (cardCode[0] == 'C') _card = Deck.AllActions[pos];
+                else if (cardCode[0] == 'O') _card = Deck.AllObjectives[pos];
+
+                if (_card != null)
+                {
+                    type = _card.type;
+                    title = _card.title;
+                    code = _card.code;
+                    flavorText = _card.flavorText ?? "";
+                    cardScript = _card.cardScript;
+                    cardPrefab = _card.cardPrefab;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new ArgumentException("Card::Card -- Copy construction failed. Code not found in Global deck.", nameof(cardCode));
+            }
         }
     }
 }
