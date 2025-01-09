4de252f42ad28df321adb2c64fbbc3cec51bbe61
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2025-01-08 21:46:39 -0500

    Update M1TheLongExile.cs
    WIP

--- Assets/Scripts/TitovCore/Cards/Milestones/M1TheLongExile.cs
+++ Assets/Scripts/TitovCore/Cards/Milestones/M1TheLongExile.cs
@@ -7,10 +7,18 @@ namespace TitovCore.Cards.Milestones
 {
     public class M1TheLongExile : MilestoneCard
     {
+        
         public void EnlistTheirSoldiers()
         {
+            int cost = 4;
+            ResourceType rt = ResourceType.Influence; 
             // Cost 4 Influence
+            if (GameState.Instance.SpendResource(ResourceType.Influence, cost))
+            {
+                      
+                GameState.Instance.actions.Add(new Card("C11"));
             // Replace 1 copy of "Conscription" with "Hill Country Conscription" 
+            }
             Debug.Log("M1TheLongExile::EnlistTheirSoldiers -- Executed");
         }
         
